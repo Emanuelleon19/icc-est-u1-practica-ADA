3fa9b780debe63ae347d3780ffe82d13e46aa788
jhonatanT04 <venotacu@gmail.com>
2024-11-13 12:03:55 -0800

    Acabar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,42 @@
+import java.util.Random;
+import java.util.Arrays;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        Random random = new Random();
+        MetodosOrdenamiento mO = new MetodosOrdenamiento();
+
+        // Tamaños de los arreglos
+        int[] tamaños = {10, 100, 1000, 5000, 10000, 30000};
         
+        int[] base = new int[10];
+        for (int i = 0; i < base.length; i++) {
+            base[i] = random.nextInt(30001);  
+        }
+        
+        int[] arregloPrevio = Arrays.copyOf(base, base.length);  
+        
+        for (int tamaño : tamaños) {
+            int[] arreglo = new int[tamaño];
+            
+            for (int i = 0; i < arregloPrevio.length; i++) {
+                arreglo[i] = arregloPrevio[i];
+            }
+            
+            for (int i = arregloPrevio.length; i < tamaño; i++) {
+                arreglo[i] = random.nextInt(30001);  
+            }
+            
+            arregloPrevio = Arrays.copyOf(arreglo, Math.min(10000, tamaño)); 
+            
+        }
+        System.out.println("");
+        long startTimeBinaria = System.nanoTime();
+        mO.sortBubbleAva(arregloPrevio);
+        long endTimeBinaria = System.nanoTime();
+        System.out.println("Tiempo de ejecucion: " + (endTimeBinaria - startTimeBinaria) + " nanosegundos");
+        System.out.println("");
     }
 }
+
+
--- src/MetodosOrdenamiento.java
+++ src/MetodosOrdenamiento.java
@@ -1,3 +1,23 @@
 public class MetodosOrdenamiento {
-    
+    public int[] sortBubbleAva(int[] arreglo){
+        int n = arreglo.length;
+        for(int i = 0; i < n; i++){
+            for (int j = 0; j < n-1-i; j++) {
+                if(arreglo[j] > arreglo[j+1]){
+
+                    //intercambio
+                    int aux = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = aux;         
+                }  
+            }
+        }
+        return arreglo;
+    }
+
+    public void printArreglo(int[] arreglo) {
+        for (int num : arreglo) {
+            System.out.print(num + " ");            
+        }
+    }
 }
